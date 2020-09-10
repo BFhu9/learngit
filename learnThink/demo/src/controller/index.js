f4aabb3bd663ecf054207ab41952a725d61eaacf
BFhu9 <46189004+BFhu9@users.noreply.github.com>
2020-09-10 14:13:05 +0800

    1

--- learnThink/demo/src/controller/index.js
+++ learnThink/demo/src/controller/index.js
@@ -13,20 +13,8 @@ module.exports = class extends Base {
 	async updateAction() {
 		let Cstu = this.get();
 		console.log(Cstu);
-		if (Cstu) {
-			let rmsg = this.model('useStu').updateStu(Cstu.name, Cstu.msg);
-			console.log(rmsg);
-			let stuList = await this.model('useStu').getStu();
-			let obj = {
-				head: '这就是thinkJS嘛？？',
-				stuList
-			}
-			this.assign(obj);
-			console.log(1);
-			return this.display();
-		} else {
-			console.log(2)
-			return this.display();
-		}
+		let rmsg = this.model('useStu').updateStu(Cstu.name, Cstu.msg);
+		console.log(rmsg);
+		return this.display();
 	}
 }
